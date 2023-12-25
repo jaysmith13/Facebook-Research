e677f7a91b94e9dfaf6a5f0e1dd127a1426b1fb9
jaysmith13 <janeasmith13@gmail.com>
2023-12-25 15:33:17 -0700

    Update coco.py

--- datasets/coco.py
+++ datasets/coco.py
@@ -4,3 +4,16 @@ COCO dataset which returns image_id for evaluation.
 
 Mostly copy-paste from https://github.com/pytorch/vision/blob/13b35ff/references/detection/coco_utils.py
 """
+
+from pathlib import Path
+
+import torch
+import torch.utils.data
+import torchvision
+from pycocotools import mask as coco_mask
+
+import datasets.transforms as T
+
+class CocoDetection(torchvision.dataset.CocoDetection):
+    def __init__(self) -> None:
+        super().__init__()
